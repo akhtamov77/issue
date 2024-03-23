ed8ac8f84ed3422613803d5032104621bbbdcb20
akhtamov77 <shaxzodgithub2255@gmail.com>
2024-03-23 12:42:18 +0500

    issue
    php da masalalar

---
+++ begin.php
@@ -0,0 +1,32 @@
+<?php
+//kvadrat tomoni berilgan uni perimetri aniqlansin.
+$a=4;
+$p=4*$a;
+echo "kvadrat tomoni ".$p.'<br>';
+
+//2- kvadratning tomoni a berilgan.uning yuzasi aniqlansin.
+$a = 4;
+$s= $a*$a;
+echo "kvadrat yuzi ".$a.'<br>';
+
+//3-to`gri to`rtburchakning tomonlari ava b berilganuning yuzasi va perimetri aniqlansin
+$a=4;
+$b=3;
+$s = $a*$b;
+$p = 2*($a+$b);
+echo "to`rtburchak yuzi ".$s.'<br>';
+echo "to`rtburchak perimetri ".$p.'<br>';
+
+//4-aylana diametri berilgan d berilgan uning uzunligi aniqlansin.
+
+$d=5;
+$l=3.14*$d;
+echo "aylana uzunligi ".$l.'<br>';
+
+//5-kubning yo tomoni a berlgan uning hajmi va to`la sirti aniqlansin.
+
+$a=7;
+$v=$a*$a*$a;
+$s=6*$a*$a;
+echo "kub hajmi ".$v.'<br>';
+echo "kub to`la sirti ".$s.'<br>';
