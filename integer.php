a5a3941c948885cffc7cecee9f0f2e5cd1568216
akhtamov77 <164538498+akhtamov77@users.noreply.github.com>
2024-03-23 13:22:16 +0500

    integer
    integer masalalsi

---
+++ integer.php
@@ -0,0 +1,26 @@
+<?php
+//1
+$l=100;
+$u=100/100;
+echo "undagi metrlar soni ".$u.'<br>';
+//2
+$m=920;
+$t=920/1000;
+echo "undagi tonnalar soni ".$t.'<br>';
+//3
+$b=2048;
+$kb=2048/1024;
+echo "fayldagi kb lar soni ".$kb.'<br>';
+//4
+$a=12;
+$b=5;
+$c = $a/$b;
+echo $c.'<br>';
+
+//5
+$a=12;
+$b=5;
+$c=floor($a/$b);
+$d = $a%$b;
+echo "joylashtirish mumkin ".$c.'<br>';
+echo "joylashmagan qism ".$d.'<br>';
